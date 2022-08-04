ffe4c4f0ceeb48b3a6bd9bdd65f71cd6c404afda
Iano-theDev <sinceianmike@gmail.com>
2022-08-04 11:20:20 +0300

    save work

---
+++ models/base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is base_model module
+Contains thc BaseModel class
+"""
+
+class BaseModel:
+    """The Basemodel class from which future classes will be derived"""
+    
+    def __init__(self, *args, **kwargs):
+        """Initialization of the base model"""
+        

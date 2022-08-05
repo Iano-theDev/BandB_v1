953996e860949d88fbf294fcc001467eec1b8d07
Iano-theDev <sinceianmike@gmail.com>
2022-08-05 23:40:20 +0300

    Re-write baseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -4,43 +4,22 @@ This is base_model module
 Contains thc BaseModel class
 """
 
+from datetime import datetime
+import uuid
+import models
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+
 class BaseModel:
     """The Basemodel class from which future classes will be derived"""
     
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if hasattr(self, "created_at") and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            if hasattr(self, "updated_at") and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
-            models.storage.save()
-        
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+    
     def __str__(self):
-        """A string representation for the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.dict)
-
-    def save(self):
-        """Updates the attribute 'updated_at' with the current datetime"""
-        self.updated_at = datetime.now()
-        models.strage.save()
-    
-    def to_dict(self):
-        """returns a dictionary containing all keys/values of  the instance"""
-        new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] = self.__class__.__name__
-        return new_dict
-          
-    
+        """Returns a string representation of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)

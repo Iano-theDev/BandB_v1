c18bee9a1a8e13efca8338061a18c0ef155d8eb4
Iano-theDev <sinceianmike@gmail.com>
2022-08-07 07:41:16 +0300

    import var storage to link BaseModel to FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
 This is base_model module
-Contains thc BaseModel class
+Contains the BaseModel class
 """
 
 from datetime import datetime
 import uuid
 import models
+import storage
+
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -29,6 +31,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
+            storage.save()
 
     def __str__(self):
         """Returns a string representation of the BaseModel class"""
@@ -38,6 +42,7 @@ class BaseModel:
     def save(self):
         """updates the public instance updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Method that returns a dictionary containg all
